e5bfcab916d0d8bd8ceaf57557e7c9b7cfb351aa
david santos <david_pereira-16@hotmail.com>
2020-08-11 16:08:01 -0300

    Twitter clone profile and main container

---
+++ src/components/ProfilePage/index.tsx
@@ -0,0 +1,52 @@
+import React from 'react';
+
+import { 
+  Container,
+  Banner,
+  Avatar,
+  ProfileData,
+  LocationIcon,
+  CakeIcon,
+  Followage,
+  EditButton
+} from './styles';
+
+const ProfilePage: React.FC = () => {
+  return (
+    <Container>
+      <Banner>
+        <Avatar />
+      </Banner>
+      <ProfileData>
+        <EditButton outlined>Editar perfil</EditButton>
+        <h1>David Ackerman</h1>
+        <h2>@DavidAckerman</h2>
+
+        <p>
+          Studant at <a href="https://rocketseat.com.br">@RocketSeat</a>
+        </p>
+        <ul>
+          <li>
+            <LocationIcon />
+            São Paulo,
+            Brasil
+          </li>
+          <li>
+            <CakeIcon />
+            Nascido em 19 de maio de 1999
+          </li>
+        </ul>
+        <Followage>
+          <span>
+            seguindo <strong>80</strong>
+          </span>
+          <span>
+            <strong>93</strong> seguidores
+          </span>
+        </Followage>
+      </ProfileData>
+    </Container>
+  );
+}
+
+export default ProfilePage;
